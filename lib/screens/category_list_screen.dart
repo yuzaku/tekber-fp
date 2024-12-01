71dd97ec85b9a78345bf162c57b6d26a6a0ce9da
Riffals <3rdriffals@gmail.com>
2024-12-01 09:26:05 +0700

    bismillah bisa

--- lib/screens/category_list_screen.dart
+++ lib/screens/category_list_screen.dart
@@ -1,7 +1,163 @@
+// import 'package:flutter/material.dart';
+// import 'password_list_screen.dart';
+// import 'password_add_screen.dart';
+// import '../main.dart'; // Import the PasswordEntry class
+// import 'login_1.dart';
+
+// class CategoryListScreen extends StatefulWidget {
+//   const CategoryListScreen({Key? key}) : super(key: key);
+
+//   @override
+//   _CategoryListScreenState createState() => _CategoryListScreenState();
+// }
+
+// class _CategoryListScreenState extends State<CategoryListScreen> {
+//   List<PasswordEntry> _passwords = [];
+//   Set<String> _categories = {};
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _loadPasswords();
+//   }
+
+//   Future<void> _loadPasswords() async {
+//     final loadedPasswords = await PasswordEntry.loadFromStorage();
+//     setState(() {
+//       _passwords = loadedPasswords;
+//       _categories = _passwords.map((e) => e.category).toSet();
+//     });
+//   }
+
+//   void _navigateToPasswordList(String category) {
+//     Navigator.push(
+//       context,
+//       MaterialPageRoute(
+//         builder: (context) => PasswordListScreen(
+//           category: category,
+//           passwords: _passwords,
+//         ),
+//       ),
+//     );
+//   }
+
+//   Future<void> _deletePassword(PasswordEntry entry) async {
+//     await PasswordEntry.deleteEntry(entry);
+//     await _loadPasswords();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       appBar: AppBar(
+//         toolbarHeight: 120,
+//         elevation: 0,
+//         backgroundColor: Colors.white,
+//         title: Row(
+//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//           children: [
+//             Row(
+//               children: [
+//                 Image.asset(
+//                   'assets/logo_efo_appbar.png',
+//                   width: 95,
+//                   height: 95,
+//                 ),
+//                 const Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: [
+//                     Text(
+//                       'ENCRYPTED',
+//                       style: TextStyle(
+//                         fontSize: 20,
+//                         fontWeight: FontWeight.w800,
+//                         color: Color(0xff1C4475),
+//                       ),
+//                     ),
+//                     Text(
+//                       'FILE',
+//                       style: TextStyle(
+//                         fontSize: 20,
+//                         fontWeight: FontWeight.w800,
+//                         color: Color(0xff1C4475),
+//                       ),
+//                     ),
+//                     Text(
+//                       'ORGANIZER',
+//                       style: TextStyle(
+//                         fontSize: 20,
+//                         fontWeight: FontWeight.w800,
+//                         color: Color(0xff1C4475),
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               ],
+//             ),
+//             GestureDetector(
+//               onTap: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(
+//                     builder: (context) => AddPasswordScreen(
+//                       onSave: (entry) async {
+//                         await PasswordEntry.addEntry(entry);
+//                         await _loadPasswords();
+//                       },
+//                     ),
+//                   ),
+//                 );
+//               },
+//               child: Image.asset(
+//                 'assets/add.png',
+//                 width: 75,
+//                 height: 75,
+//                 fit: BoxFit.contain,
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//       body: ListView(
+//         children: _categories.map((category) {
+//           return Container(
+//             color: Colors.white,
+//             padding: const EdgeInsets.symmetric(horizontal: 16.0),
+//             child: Card(
+//               color: const Color(0xff1C4475),
+//               margin: const EdgeInsets.symmetric(vertical: 8),
+//               child: Padding(
+//                 padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+//                 child: ListTile(
+//                   title: Text(
+//                     category,
+//                     style: const TextStyle(color: Colors.white),
+//                   ),
+//                   onTap: () => _navigateToPasswordList(category),
+//                   leading: Image.asset(
+//                     'assets/logo_ig.png',
+//                   ),
+//                   trailing: Image.asset(
+//                     'assets/arrow.png',
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           );
+//         }).toList(),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'password_list_screen.dart';
 import 'password_add_screen.dart';
 import '../main.dart'; // Import the PasswordEntry class
+import 'login_1.dart';
+import 'regist_1.dart';
+import 'regist_3.dart';
+import 'regist_5.dart';
 
 class CategoryListScreen extends StatefulWidget {
   const CategoryListScreen({Key? key}) : super(key: key);
@@ -118,33 +274,91 @@ class _CategoryListScreenState extends State<CategoryListScreen> {
           ],
         ),
       ),
-      body: ListView(
-        children: _categories.map((category) {
-          return Container(
-            color: Colors.white,
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Card(
-              color: const Color(0xff1C4475),
-              margin: const EdgeInsets.symmetric(vertical: 8),
-              child: Padding(
-                padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
-                child: ListTile(
-                  title: Text(
-                    category,
-                    style: const TextStyle(color: Colors.white),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: _categories.map((category) {
+                return Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Card(
+                    color: const Color(0xff1C4475),
+                    margin: const EdgeInsets.symmetric(vertical: 8),
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                      child: ListTile(
+                        title: Text(
+                          category,
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        onTap: () => _navigateToPasswordList(category),
+                        leading: Image.asset(
+                          'assets/logo_ig.png',
+                        ),
+                        trailing: Image.asset(
+                          'assets/arrow.png',
+                        ),
+                      ),
+                    ),
                   ),
-                  onTap: () => _navigateToPasswordList(category),
-                  leading: Image.asset(
-                    'assets/logo_ig.png',
-                  ),
-                  trailing: Image.asset(
-                    'assets/arrow.png',
-                  ),
-                ),
-              ),
+                );
+              }).toList(),
             ),
-          );
-        }).toList(),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Navigasi ke halaman login_1.dart
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Login_1()),
+                );
+              },
+              child: const Text('Go to Login Page'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Navigasi ke halaman login_1.dart
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Regist_1()),
+                );
+              },
+              child: const Text('Go to Regist_1 Page'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Navigasi ke halaman login_1.dart
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Regist_3()),
+                );
+              },
+              child: const Text('Go to Regist_3 Page'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Navigasi ke halaman login_1.dart
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Regist_5()),
+                );
+              },
+              child: const Text('Go to Regist_5 Page'),
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/screens/login_1.dart
@@ -0,0 +1,205 @@
+import 'package:flutter/material.dart';
+
+class Login_1 extends StatelessWidget {
+  const Login_1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white, // Latar belakang putih
+        elevation: 0, // Hilangkan bayangan
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black), // Tombol back
+          onPressed: () {
+            Navigator.pop(context); // Kembali ke halaman sebelumnya
+          },
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center, // Posisikan di tengah horizontal
+          children: [
+            const SizedBox(height: 20), // Beri jarak dari atas
+            // Logo Section
+            Center( // Gunakan Center untuk memastikan elemen dalam Column berada di tengah horizontal
+              child: Image.asset(
+                'assets/logo_nama.png',
+                width: 300,
+                height: 300,
+              ),
+            ),
+            // const SizedBox(height: 20),
+            const Text(
+              'Login', 
+              style: TextStyle(
+                fontSize: 50, // Ukuran font
+                fontWeight: FontWeight.bold, // Tebal
+                fontStyle: FontStyle.italic, // Italic
+                color: Color(0xff8A9586), // Warna #8A9586
+                fontFamily: 'Montserrat', // Font Montserrat
+              ),
+            ),
+            const Spacer(),
+            // Authentication Form Container - Sticks to the bottom
+            Container(
+              padding: const EdgeInsets.only(
+                  top: 8, bottom: 8, left: 45, right: 45),
+              margin: const EdgeInsets.symmetric(horizontal: 30),
+              decoration: const BoxDecoration(
+                color: Color(0xff82A1D1),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(8),
+                  topRight: Radius.circular(8),
+                ),
+              ),
+              // Authentication Form
+              child: const Column(
+                children: [
+                  Text(
+                    'Forgot Passcode?',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontFamily: 'Montserrat', // Font Montserrat
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // Input Form Container
+            Container(
+              padding: const EdgeInsets.only(
+                top: 15,
+                left: 20,
+                right: 20,
+                bottom: 15),
+              margin: const EdgeInsets.symmetric(horizontal: 30),
+              decoration: BoxDecoration(
+                color: const Color(0xff8A9586),
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(8),
+                  topRight: Radius.circular(8)),
+              ),
+              child: Column(
+                children: [
+                  // Full Name Text
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Username',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontFamily: 'Montserrat', // Font Montserrat
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  // Full Name Input
+                  TextField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: InputBorder.none, // Remove outline border
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8), // Reduced vertical padding for smaller height
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide.none, // No border side
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide.none, // No border side
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  // Birthdate Text
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Passcode',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontFamily: 'Montserrat', // Font Montserrat
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  // Birthdate Input
+                  TextField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: InputBorder.none, // Remove outline border
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8), // Reduced vertical padding for smaller height
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide.none, // No border side
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide.none, // No border side
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 70),
+                  // Confirm Button
+                  TextButton(
+                    onPressed: () {}, // Aksi tombol
+                    style: TextButton.styleFrom(
+                      backgroundColor: const Color(0xff82A1D1),
+                      foregroundColor: Colors.white,
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8), // Border radius of 8
+                      side: const BorderSide(color: Color(0xff82A1D1), width: 2), // Border color and width
+                    ), // Text color
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween, 
+                      children: [
+                        const Text(
+                          'Continue',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20, 
+                            color: Colors.white,
+                            fontFamily: 'Montserrat', // Font Montserrat
+                          ),
+                        ),
+                        Image.asset(
+                          'assets/arrow_kategori_b.png', 
+                          width: 50, 
+                          height: 50,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  // Footer Text
+                  const Text(
+                    '“Organize smarter, secure better—\nEFO has you covered.”',
+                    style: TextStyle(
+                      fontSize: 14, // Ukuran font lebih kecil untuk teks penutup
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic, // Italic
+                      color: Colors.white,
+                      fontFamily: 'Montserrat', // Font Montserrat
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
