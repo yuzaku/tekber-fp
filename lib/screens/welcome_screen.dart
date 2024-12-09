1ddd3da86b1d3e98fd17547fc37c94df5aabde6d
yuzaku <yanuaraudrey55@gmail.com>
2024-12-09 21:05:11 +0700

    refactor: remove unnecessary import

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:passmanager/screens/category_list_screen.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: WelcomeScreen(),
-    );
-  }
-}
-
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
 
